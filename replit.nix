bac5b8e69eaef8c9ccbc177a6410869c53cfdeb4
Ianmuhandichi <159758557+Ianmuhandichi@users.noreply.github.com>
2026-01-03 03:21:48 +0300

    Modify dependencies and add environment variables
    Updated dependencies and environment variables in replit.nix

--- replit.nix
+++ replit.nix
@@ -1,8 +1,14 @@
-{pkgs}: {
+{ pkgs }: {
   deps = [
-    pkgs.nodejs-18_x
-    pkgs.ffmpeg
-    pkgs.libwebp
     pkgs.python3
+    pkgs.nodejs_20
+    pkgs.gcc
+    pkgs.openssl
+    pkgs.sqlite
   ];
+  
+  env = {
+    LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+    PYTHONPATH = "${pkgs.python3}/lib/python3.10/site-packages/";
+  };
 }
